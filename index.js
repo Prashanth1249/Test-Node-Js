dd0ed1033e903017c6f276bb262e7888a8628220
Prashanth Pottollla <106907242+Prashanth1249@users.noreply.github.com>
2025-10-23 18:35:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -33,11 +33,11 @@ app.get("/getRiders", async (req, res) => {
 });
 
 // Start server
-app.listen(3000, () => {
-  console.log("✅ Server running on http://localhost:3000");
-});
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`✅ Server running on http://localhost:${PORT}`);
+// app.listen(3000, () => {
+//   console.log("✅ Server running on http://localhost:3000");
 // });
+ const PORT = process.env.PORT || 3000;
+
+ app.listen(PORT, () => {
+   console.log(`✅ Server running on http://localhost:${PORT}`);
+ });
