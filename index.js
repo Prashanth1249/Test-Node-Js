4b22cc09e7e5c22dc523e131003b89a57812ce29
Prashanth Pottollla <106907242+Prashanth1249@users.noreply.github.com>
2025-10-23 20:11:42 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,4 +49,4 @@ app.get("/getRiders", async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
